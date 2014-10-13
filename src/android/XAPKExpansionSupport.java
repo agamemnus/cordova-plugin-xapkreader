ea3122a8dd3af1b897b50449360995b723b9df02
agamemnus <agamemnus@gmail.com>
2014-10-13 00:00:22 -0400

    Update XAPKExpansionSupport.java

--- src/android/XAPKExpansionSupport.java
+++ src/android/XAPKExpansionSupport.java
@@ -1,5 +1,5 @@
 package org.apache.cordova.xapkreader;
-
+ 
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
@@ -10,55 +10,50 @@ import java.util.regex.Pattern;
 
 import android.content.Context;
 import android.os.Environment;
-import android.util.Log;
 
 public class XAPKExpansionSupport {
-	// The shared path to all app expansion files
-	private final static String EXP_PATH = "/Android/obb/";
- private static final String LOG_TAG = "XAPKExpansionSupport";
+ // The shared path to all app expansion files
+ private final static String EXP_PATH = "/Android/obb/";
  
-	static String[] getAPKExpansionFiles (Context ctx, int mainVersion, int patchVersion) {
-  Log.e (LOG_TAG, "getAPKExpansionFiles");
-		String packageName = ctx.getPackageName ();
-		Vector<String> ret = new Vector<String> ();
+ static String[] getAPKExpansionFiles (Context ctx, int mainVersion, int patchVersion) {
+  String packageName = ctx.getPackageName ();
+  Vector<String> ret = new Vector<String> ();
   
   // Exit out if there's no way to get to the shared storage directory.
-		if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) return ret.toArray (new String[0]);
+  if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) return ret.toArray (new String[0]);
   
-		// Build the full path to the app's expansion files.
-		File root = Environment.getExternalStorageDirectory ();
+  // Build the full path to the app's expansion files.
+  File root = Environment.getExternalStorageDirectory ();
   String expPathString = root.toString() + EXP_PATH + packageName;
-		File expPath = new File(expPathString);
+  File expPath = new File(expPathString);
   
-		// Check that the expansion file path exists.
-		if (!expPath.exists()) return ret.toArray (new String[0]);
+  // Check that the expansion file path exists.
+  if (!expPath.exists()) return ret.toArray (new String[0]);
   
   // Get a list of files in this directory.
   ArrayList<String> fileList = listFiles (expPathString);
   
   // Check if either the main or path files exist.
-  int[]    versionNumber = {patchVersion, mainVersion};
-  String[] versionCheck  = {"patch"     , "main"};
+  int[]    versionNumber = {mainVersion, patchVersion};
+  String[] versionCheck  = {"main"     , "patch"};
   for (int i = 0; i < versionNumber.length; i++) {
    String strPath = "";
    int currentVersionNumber = versionNumber[i];
-   Log.e (LOG_TAG, currentVersionNumber+"");
    if (currentVersionNumber > 0) {
-   	strPath = expPath + File.separator + versionCheck[i] + "." + currentVersionNumber + "." + packageName + ".obb";
+    strPath = expPath + File.separator + versionCheck[i] + "." + currentVersionNumber + "." + packageName + ".obb";
    } else if (currentVersionNumber == 0) {
     // Find if there is a "patch" or "main" file in the files list.
     for (String file : fileList) {
-     Log.e (LOG_TAG, file);
      if (file.indexOf(expPath + File.separator + versionCheck[i]) == 0) {strPath = file; break;}
-		 	}
+    }
    }
-		 if (new File(strPath).isFile()) ret.add(strPath);
+   if (new File(strPath).isFile()) ret.add(strPath);
   }
   
-		String[] retArray = new String[ret.size()];
-		ret.toArray(retArray);
-		return retArray;
-	}
+  String[] retArray = new String[ret.size()];
+  ret.toArray(retArray);
+  return retArray;
+ }
  
  // Get all the files in a directory.
  static public ArrayList<String> listFiles (String directoryName) {
@@ -71,7 +66,7 @@ public class XAPKExpansionSupport {
   return files;
  }
  
-	static public XAPKZipResourceFile getResourceZipFile (String[] expansionFiles) throws IOException {
+ static public XAPKZipResourceFile getResourceZipFile (String[] expansionFiles) throws IOException {
   XAPKZipResourceFile apkExpansionFile = null;
   for (String expansionFilePath : expansionFiles) {
    if (apkExpansionFile == null) {
@@ -81,10 +76,10 @@ public class XAPKExpansionSupport {
    }
   }
   return apkExpansionFile;
-	}
-	
-	static public XAPKZipResourceFile getAPKExpansionZipFile (Context ctx, int mainVersion, int patchVersion) throws IOException {
-		String[] expansionFiles = getAPKExpansionFiles (ctx, mainVersion, patchVersion);
-		return getResourceZipFile (expansionFiles);
-	}
+ }
+ 
+ static public XAPKZipResourceFile getAPKExpansionZipFile (Context ctx, int mainVersion, int patchVersion) throws IOException {
+  String[] expansionFiles = getAPKExpansionFiles (ctx, mainVersion, patchVersion);
+  return getResourceZipFile (expansionFiles);
+ }
 }
