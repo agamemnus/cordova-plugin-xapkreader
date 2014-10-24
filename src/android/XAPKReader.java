6df9d66817dd39ee98dacb9a5de81527c6987f8f
agamemnus <agamemnus@gmail.com>
2014-10-24 18:31:40 -0400

    Update XAPKReader.java

--- src/android/XAPKReader.java
+++ src/android/XAPKReader.java
@@ -20,39 +20,39 @@ public class XAPKReader extends CordovaPlugin {
   
   // Get some data from the xapkreader.xml file.
   String[][] xmlData = new String[][] {
-   {"main_version"           , "integer"},
-   {"patch_version"          , "integer"},
-   {"main_file_size"         , "integer"},
-   {"patch_file_size"        , "integer"},
-   {"expansion_authority"    , "string"},
-   {"text_downloading_assets", "string"},
-   {"text_preparing_assets"  , "string"},
-   {"text_download_failed"   , "string"},
-   {"text_error"             , "string"},
-   {"text_close"             , "string"}
+   {"xapk_main_version"           , "integer"},
+   {"xapk_patch_version"          , "integer"},
+   {"xapk_main_file_size"         , "integer", "long"},
+   {"xapk_patch_file_size"        , "integer", "long"},
+   {"xapk_expansion_authority"    , "string"},
+   {"xapk_text_downloading_assets", "string"},
+   {"xapk_text_preparing_assets"  , "string"},
+   {"xapk_text_download_failed"   , "string"},
+   {"xapk_text_error"             , "string"},
+   {"xapk_text_close"             , "string"}
   };
   int curlen = xmlData.length;
   for (int i = 0; i < curlen; i++) {
    int currentId = cordova.getActivity().getResources().getIdentifier (xmlData[i][0], xmlData[i][1], packageName);
    if (xmlData[i][1] == "bool")    {bundle.putBoolean(xmlData[i][0], cordova.getActivity().getResources().getBoolean(currentId)); continue;}
    if (xmlData[i][1] == "string")  {bundle.putString (xmlData[i][0], cordova.getActivity().getResources().getString (currentId)); continue;}
-   if (xmlData[i][1] == "integer") {bundle.putInt    (xmlData[i][0], cordova.getActivity().getResources().getInteger(currentId)); continue;}
+   if (xmlData[i][1] == "integer") {
+    if ((xmlData[i].length == 2) || (xmlData[i][2] == "integer")) {
+     bundle.putInt  (xmlData[i][0], cordova.getActivity().getResources().getInteger(currentId)); continue;
+    }
+    if (xmlData[i][2] == "long") {
+     bundle.putLong (xmlData[i][0], cordova.getActivity().getResources().getInteger(currentId)); continue;
+    }
+   }
   }
   
   // Send data to the ContentProvider instance.
   ContentResolver cr = cordova.getActivity().getApplicationContext().getContentResolver();
-  String expansionAuthority = bundle.getString("expansion_authority", "");
+  String expansionAuthority = bundle.getString("xapk_expansion_authority", "");
   cr.call (Uri.parse("content://" + expansionAuthority), "set_expansion_file_version_data", null, bundle);
   
   // Set the public key.
-  XAPKDownloaderService.BASE64_PUBLIC_KEY = bundle.getString("google_play_public_key", "");
-  
-  // Load the expansion file.
-  try {
-   webView.getResourceApi().openForRead(Uri.parse(expansionAuthority), true);
-  } catch (IOException e) {
-   e.printStackTrace ();
-  }
+  XAPKDownloaderService.BASE64_PUBLIC_KEY = bundle.getString("xapk_google_play_public_key", "");
   
   cordova.getActivity().runOnUiThread (new Runnable() {
    @Override public void run () {
