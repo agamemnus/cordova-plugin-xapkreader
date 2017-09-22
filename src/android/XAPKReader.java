afece4c96b3817e9987a3d11a0bb65e4cc899c3d
Chip Bell <cbell@pbs.org>
2017-09-22 15:36:46 -0400

    Fixing permission issues in android 7

--- src/android/XAPKReader.java
+++ src/android/XAPKReader.java
@@ -87,7 +87,7 @@ public class XAPKReader extends CordovaPlugin {
         // Workaround for Android 6 bug wherein downloaded OBB files have the wrong file permissions
         // and require WRITE_EXTERNAL_STORAGE permission
         if (
-                Build.VERSION.SDK_INT == 23
+                Build.VERSION.SDK_INT >= 23
                 && !cordova.hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         ) {
             // We need the permission; so request it (asynchronously, callback is onRequestPermissionsResult)
